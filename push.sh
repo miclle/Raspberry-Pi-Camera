a9ffbf3321289daf6f9c708f6d87b2db8d67334e
Miclle <miclle.zheng@gmail.com>
2015-01-11 12:18:35 +0800

    Fix bug

--- push.sh
+++ push.sh
@@ -8,6 +8,6 @@ killall raspivid
 
 while true; do
   raspivid -t 99999999 -w 960 -h 540 -fps 25 -b 500000 -o - | ffmpeg -i - -vcodec copy -an -f flv rtmp://pili-in.qiniu.com/livestream/o7hghf8k?stream_key=c0ab7734-2c29-4b76-b982-0fda3f242337
-do
+done
 
 ##nohup sh run.sh -P run.pid >> run.log 2>&1 &
