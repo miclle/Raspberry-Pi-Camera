cd3d33566a7775b4da674762d13fe236145429e1
Miclle <miclle.zheng@gmail.com>
2015-01-15 11:14:36 +0800

    Update push address.

--- push.sh
+++ push.sh
@@ -4,7 +4,7 @@
 killall raspivid
 
 while true; do
-  raspivid -t 99999999 -w 960 -h 540 -fps 25 -b 500000 -o - | /usr/local/bin/ffmpeg -i - -vcodec copy -an -f flv rtmp://pili-in.qiniu.com/livestream/o7hghf8k?stream_key=c0ab7734-2c29-4b76-b982-0fda3f242337
+  raspivid -t 99999999 -w 960 -h 540 -fps 25 -b 500000 -o - | /usr/local/bin/ffmpeg -i - -vcodec copy -an -f flv rtmp://pili-in.qiniu.com/livestream/o7hghf8k?key=c0ab7734-2c29-4b76-b982-0fda3f242337
 done
 
 ##nohup sh run.sh -P run.pid >> run.log 2>&1 &
